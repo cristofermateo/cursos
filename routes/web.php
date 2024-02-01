36cebcd74317684ffb9d6bc249840e52390e2512
cristofermateo <christocheter@gmail.com>
2024-01-31 19:21:39 -0600

    comenzandotask

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\NoticiaController;
-use App\Http\Controllers\CursoController;
+use App\Http\Controllers\TaskController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,30 +13,9 @@ use App\Http\Controllers\CursoController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get("/", NoticiaController::class);
-
-Route::get("cursos", [CursoController::class, 'index']);
-
-Route::get("cursos/create", [CursoController::class, 'create']);
-
-Route::get("cursos/{curso}", [CursoController::class, 'show']);
-
-/*
-Route::get("cursos/{curso}/{categoria}",function($curso , $categoria = null){
-
-dif($categoria){
-
-    return "bienvenido al curso: $curso, de la categoria $categoria";
-}else{
-    return "bienvenido al curso: $curso";
-}
-
-
-
-    return "bienvenido al curso: $curso, de la categoria $categoria";
+Route::get('/', function () {
+      return view('welcome');
 });
-*/
 
-
+Route::resource('tasks',TaskController::class);
 
